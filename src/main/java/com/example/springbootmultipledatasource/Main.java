c663b2e7d47a36e401f1f4b80ff773fd8551a927
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2022-10-19 18:37:08 +0530

    19 Octo 7 PM

---
+++ src/main/java/com/example/springbootmultipledatasource/Main.java
@@ -0,0 +1,7 @@
+package com.example.springbootmultipledatasource;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Spring Boot Application");
+    }
+}
