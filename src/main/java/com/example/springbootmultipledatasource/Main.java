5c1a1e4845a48d6e5b26f3258a4469d2d9dd27e7
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2022-10-19 18:39:21 +0530

    Revert "19 Octo 7 PM"
    This reverts commit c663b2e7d47a36e401f1f4b80ff773fd8551a927.

--- src/main/java/com/example/springbootmultipledatasource/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootmultipledatasource;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starting Spring Boot Application");
-    }
-}
